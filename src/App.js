ca2e0a87cb39f6b564ead37c9db47c1d88eb4a0b
demichev-artur <artur.demichev03@mail.ru>
2022-07-20 21:52:55 +0600

    feat: HOME-WORK 20_07

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
+import Raiting from "./components/Raiting";
+import Tags from "./components/Tags";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+
+    let tagsOne = [
+        {title: 'Тег №1', href: 'http://link1.ru'},
+        {title: 'Тег №2', href: 'http://link2.ru'},
+        {title: 'Тег №3', href: 'http://link3.ru'},
+    ]
+
+    let tagsTwo = [
+        {title: 'Какая-то ссылка №1', href: 'http://link1.ru'},
+        {title: 'Какая-то ссылка №2', href: 'http://link2.ru'},
+        {title: 'Какая-то ссылка №3', href: 'http://link3.ru'},
+        {title: 'Какая-то ссылка №4', href: 'http://link4.ru'},
+        {title: 'Какая-то ссылка №5', href: 'http://link5.ru'},
+        {title: 'Возможно какая-то ссылка №6', href: 'http://link6.ru'}
+    ]
+
+    let tagsTree = [
+        {title: 'Travels', href: 'http://link1.ru'},
+        {title: 'Sports', href: 'http://link2.ru'},
+        {title: 'Music', href: 'http://link3.ru'},
+        {title: 'Political', href: 'http://link4.ru'}
+    ]
+
+    return (
+
+        <div className="content">
+            <Raiting starCount={5} starRaiting={2}/>
+            <Raiting starCount={4} starRaiting={3}/>
+            <Raiting starCount={10} starRaiting={8}/>
+            <Raiting starCount={20} starRaiting={8}/>
+            <Tags tags={tagsOne}/>
+            <Tags tags={tagsTwo}/>
+            <Tags tags={tagsTree}/>
+        </div>
+
+    );
 }
 
 export default App;
